84aaeb0c76df46edef0cb59562937066af82d705
unknown <gabrielquadro@gmail.com>
2024-04-23 18:29:04 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>BarberPro seu sistema completo</title>
+    </Head>
+      <div>
+        <h1>Teste</h1>
+      </div>
+    </>
+  )
+}
